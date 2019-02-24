e9441d556cbfd7f776d4cb5739e791bd53736287
hc <542401909@qq.com>
2019-02-24 22:14:13 +0800

    Initial commit

---
+++ src/main/java/cn/icnode/Student.java
@@ -0,0 +1,22 @@
+package cn.icnode;
+
+public class Student {
+    private String name;
+    private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
